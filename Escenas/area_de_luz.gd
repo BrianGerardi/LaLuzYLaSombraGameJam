974e6bac8797fce43b3e8ad9b45fdc2c83d7afcc
Marcos Nicolas Salinas <smarcosnicolas@gmail.com>
2025-08-27 01:14:27 -0300

    init
    vida + hud

--- Escenas/area_de_luz.gd
+++ Escenas/area_de_luz.gd
@@ -1,6 +1,9 @@
 extends Node2D
 
-
+@export var daño_area1 : int = 5
+@export var daño_area2 : int = 5
+@export var daño_area3 : int = 5
+@export var daño_area4 : int = 5
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	pass # Replace with function body.
@@ -14,20 +17,21 @@ func _process(delta: float) -> void:
 func _on_area_de_daño_1_body_entered(body:Node2D) -> void:
 	if body.name != "Player":
 		return
-	Global.jugador_entro_en_area_de_luz_lvl1_signal.emit()
+	Global.jugador_entro_en_area_de_luz_lvl1_signal.emit(daño_area1)
+	#puede ser asi o usar directamente la señal de restar_vida
 
 func _on_area_de_daño_2_body_entered(body:Node2D) -> void:
 	if body.name != "Player":
 		return
-	Global.jugador_entro_en_area_de_luz_lvl2_signal.emit()
+	Global.jugador_entro_en_area_de_luz_lvl2_signal.emit(daño_area2)
 
 func _on_area_de_daño_3_body_entered(body:Node2D) -> void:
 	if body.name != "Player":
 		return
-	Global.jugador_entro_en_area_de_luz_lvl3_signal.emit()
+	Global.jugador_entro_en_area_de_luz_lvl3_signal.emit(daño_area3)
 
 func _on_area_de_daño_4_body_entered(body:Node2D) -> void:
 	if body.name != "Player":
 		return
-	Global.jugador_entro_en_area_de_luz_lvl4_signal.emit()
+	Global.jugador_entro_en_area_de_luz_lvl4_signal.emit(daño_area4)
 	
--- script global/global.gd
+++ script global/global.gd
@@ -6,11 +6,12 @@ extends Node
 
 
 signal usar_palanca_id(identificador : int)
-
-signal jugador_entro_en_area_de_luz_lvl1_signal
-signal jugador_entro_en_area_de_luz_lvl2_signal
-signal jugador_entro_en_area_de_luz_lvl3_signal
-signal jugador_entro_en_area_de_luz_lvl4_signal
+signal restar_vida(cantidad_a_restar : int) #la usamos cuando el player reciba daño, el hud esta conectada para poder actualizar la barra de vida
+signal jugador_entro_en_area_de_luz_lvl1_signal(restar_vida: int)
+signal game_over
+signal jugador_entro_en_area_de_luz_lvl2_signal(restar_vida: int)
+signal jugador_entro_en_area_de_luz_lvl3_signal(restar_vida: int)
+signal jugador_entro_en_area_de_luz_lvl4_signal(restar_vida: int)
 
 
 func _ready() -> void:
