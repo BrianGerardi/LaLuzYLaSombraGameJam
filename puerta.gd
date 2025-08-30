b6711140729e515ebfd0d0f65967140035ea43be
Marcos Nicolas Salinas <smarcosnicolas@gmail.com>
2025-08-29 21:06:38 -0300

    animacion de puerta abierta y cerrada
    agregue nodo audiostreamplayer tambien

--- puerta.gd
+++ puerta.gd
@@ -1,10 +1,9 @@
 extends StaticBody2D
 
-
 @export var id_puerta : int
 @export var sprite_puerta_abierta: Sprite2D
 @onready var collision_puerta_abierta: CollisionShape2D = %CollisionPuertaAbierta
-
+var puerta_abierta :bool = false
 
 func _ready() -> void:
 	Global.usar_palanca_id.connect(_on_usar_palanca)
@@ -21,6 +20,9 @@ func _on_usar_palanca(identificador : int):
 
 func abrir_puerta():
 	collision_puerta_abierta.set_deferred("disabled", true)
-	%AnimatedSprite2D.play("abrir")
+	if puerta_abierta == false:
+		%AnimatedSprite2D.play("abrir")
+	else:
+		%AnimatedSprite2D.play("cerrar")
+	%AudioPuerta.play() #asegurarse de haber agregado el sonido por medio del editor
 	print("puerta abierta")
-	#si hubieran animaciones tambien hay que ejecutarlas aca
