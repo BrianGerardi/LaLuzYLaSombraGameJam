942e8f69843484d52a972025f61bb51fde56865c
Marcos Nicolas Salinas <smarcosnicolas@gmail.com>
2025-09-02 01:36:20 -0300

    menu 3d y texturas retry + game over
    probar que se vean bien en todas las pc el menu 3d, ahora esta puesto como main scene

---
+++ Escenas/main_menu_3d.gd
@@ -0,0 +1,19 @@
+extends Node3D
+
+var nivel_tutorial = "res://Escenas/Niveles/NivelTutorial.tscn"
+var textura_pressed = preload("res://Assets/MenuDeInicio/PLAY_CLICK.png")
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
+
+
+func _on_boton_play_input_event(camera: Node, event: InputEvent, event_position: Vector3, normal: Vector3, shape_idx: int) -> void:
+	if (event is InputEventMouseButton and event.pressed and event.button_index == MOUSE_BUTTON_LEFT):
+		print("aaaaaa")
+		%ImgPlay.texture = textura_pressed
+		await get_tree().create_timer(0.1).timeout #para darle tiempo a que se vea la textura pressed
+		get_tree().change_scene_to_file(nivel_tutorial)
