bf28809953266b07429d5bc22d605e8259b18e01
Marcos Nicolas Salinas <smarcosnicolas@gmail.com>
2025-09-04 17:05:06 -0300

    barra de volumen casi terminada
    falta el arte nuevo y acomodar en su posicion correspondiente

--- Escenas/main_menu_3d.gd
+++ Escenas/main_menu_3d.gd
@@ -2,6 +2,7 @@ extends Node3D
 
 var nivel_tutorial = "res://Escenas/Niveles/NivelTutorial.tscn"
 var textura_pressed = preload("res://Assets/MenuDeInicio/PLAY_CLICK.png")
+var textura_normal = preload("res://Assets/MenuDeInicio/PLAY_NO_CLICK.png")
 func _ready() -> void:
 	pass # Replace with function body.
 
@@ -15,5 +16,14 @@ func _on_boton_play_input_event(camera: Node, event: InputEvent, event_position:
 	if (event is InputEventMouseButton and event.pressed and event.button_index == MOUSE_BUTTON_LEFT):
 		print("aaaaaa")
 		%ImgPlay.texture = textura_pressed
+		%AudioClick.play()
 		await get_tree().create_timer(0.1).timeout #para darle tiempo a que se vea la textura pressed
 		get_tree().change_scene_to_file(nivel_tutorial)
+
+
+func _on_boton_play_mouse_entered() -> void:
+	%ImgPlay.texture = textura_pressed
+
+
+func _on_boton_play_mouse_exited() -> void:
+	%ImgPlay.texture = textura_normal
